bd82580a890142f963c502353632d643bdb237d3
Vincent Gillaux <gillaux.vincent@gmail.com>
2016-04-07 12:22:34 +0200

    Affichage des users sur la page admin

--- src/IglesBundle/Controller/AdminController.php
+++ src/IglesBundle/Controller/AdminController.php
@@ -10,13 +10,18 @@ class AdminController extends Controller
     /**
      * @Route("/admin", name="admin")
      */
-    public function indexAction()
-    {
-    	return $this->render('IglesBundle::admin.html.twig');
-    }
-
-    // public function profile_userAction()
+    // public function indexAction()
     // {
-    //     return $this->render('IglesBundle::index.html.twig');
+    // 	return $this->render('IglesBundle::index.html.twig');
     // }
+
+
+    //Fonction pour récupérer la liste de tous les utilisateurs
+    public function usersAction() {
+
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+        return $this->render('IglesBundle::admin.html.twig', array('users' =>   $users));
+    }
 }
